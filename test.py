8183e56c8545b7c97da2767b9f01e79a9eaf4c1b
Yelisomah <yelisomahe@gmail.com>
2026-06-16 10:00:14 +0000

    fix syntax error

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import sys
 def test_app():
     result = subprocess.run([sys.executable, 'app.py'], capture_output=True, text=True)
     assert result.returncode == 0, "App did not run successfully"
-    assert "Hello, welcome to CICD pipeline project! Hurray" in result.stdout, "wrong output"
+    assert "Hello, welcome to CICD pipeline project!" in result.stdout, "wrong output"
     
 if __name__ == "__main__":
     test_app()
